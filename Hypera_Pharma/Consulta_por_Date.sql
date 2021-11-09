d0557dc76d276abcd93f791e27fdbda1bf8156ea
Victor Gonçalves <60231827+victor1cg@users.noreply.github.com>
2021-11-09 12:58:34 +0100

    Update Consulta_por_Date.sql

--- Hypera_Pharma/Consulta_por_Date.sql
+++ Hypera_Pharma/Consulta_por_Date.sql
@@ -3,15 +3,60 @@ SET @DataRel = '2020-01-01'
 
 SELECT	cd_Time			  ,
 		cd_Regional		  ,
-		cd_Setor		  ,
-		cd_Amostra		  ,
-		nr_CRM			  ,
-		uf_CRM			  ,
-		nm_Medico		  ,
-		dt_Visita		  ,
-		cd_MaterialAmostra,
-		nm_MaterialAmostra,
-		cd_Lote			  ,	
+		cd_Setor		  ,	
 		qt_Amostra
 FROM running_farma..tb_farma_amostras
 WHERE CONVERT(DATE, dt_Visita, 103) >= @DataRel
+----------------
+############ OVER - Partition
+
+SELECT Customercity, 
+       CustomerName, 
+       OrderAmount, 
+       AVG(Orderamount) OVER(PARTITION BY Customercity) AS AvgOrderAmount, 
+       MIN(OrderAmount) OVER(PARTITION BY Customercity) AS MinOrderAmount, 
+       SUM(Orderamount) OVER(PARTITION BY Customercity) TotalOrderAmount
+FROM Orders;
+
+return  to us the average by city:
+
+CITY	NAME 	ORDER	AvgOrderAmount	MinOrderAmount	TotalOrderAmount
+Austin	Joe	$950	$1000		$250		$80000
+Chicago	jose	$300	$600		$100		$7000
+
+#### PARTITION BY clause with ROW_NUMBER()
+We can use the SQL PARTITION BY clause with ROW_NUMBER() function to have a row number of each row. 
+We define the following parameters to use ROW_NUMBER with the SQL PARTITION BY clause.
+
+SELECT Customercity, 
+       CustomerName, 
+       ROW_NUMBER() OVER(PARTITION BY Customercity ORDER BY OrderAmount DESC) AS "Row Number", 
+       OrderAmount, 
+       COUNT(OrderID) OVER(PARTITION BY Customercity) AS CountOfOrders, 
+       AVG(Orderamount) OVER(PARTITION BY Customercity) AS AvgOrderAmount, 
+       MIN(OrderAmount) OVER(PARTITION BY Customercity) AS MinOrderAmount, 
+       SUM(Orderamount) OVER(PARTITION BY Customercity) TotalOrderAmount
+FROM [dbo].[Orders];
+
+CITY	NAME 	Row Number	ORDER	AvgOrderAmount	MinOrderAmount	TotalOrderAmount
+Austin	Joe	1		$950	$800		$600		$2250
+Austin  Marvin	2		$700	$800		$600		$2250
+Austin	John	3		$600	$800		$600		$2250	
+
+Chicago	jose			$300	$600		$100		$7000
+
+# Creating a CUMULATIVE SUM
+SELECT Customercity, 
+       CustomerName, 
+       OrderAmount, 
+       ROW_NUMBER() OVER(PARTITION BY Customercity
+       ORDER BY OrderAmount DESC) AS "Row Number", 
+       CONVERT(VARCHAR(20), SUM(orderamount) OVER(PARTITION BY Customercity
+       ORDER BY OrderAmount DESC ROWS BETWEEN CURRENT ROW AND 1 FOLLOWING), 1) AS CumulativeTotal,
+      
+CITY	NAME 	Row Number	ORDER	CumulativeTotal	
+Austin	Joe	1		$950	$2250		
+Austin  Marvin	2		$700	$1300		
+Austin	John	3		$600	$600			
+
+Chicago	jose	1		$300	$600		
